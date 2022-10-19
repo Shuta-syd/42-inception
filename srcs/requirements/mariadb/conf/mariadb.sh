8207cabb4aa8ea019ead1da76c2542c98132595f
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-19 20:24:53 +0900

    [mod] mariadb.sh

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -2,4 +2,11 @@
 
 echo "Hello MariaDB containcer"
 
+#to start mariadb on alpine linux
+mkdir -p /run/openrc/
+touch /run/openrc/softlevel
+/etc/init.d/mariadb setup
+rc default
+/etc/init.d/mariadb start
+
 tail -f /dev/null
