068ef6d1883fe4d6cd7361a6b9cfa8a73fda303a
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-19 22:47:00 +0900

    [mod] mariadb/conf/mariadb.sh

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -9,4 +9,10 @@ touch /run/openrc/softlevel
 rc default
 /etc/init.d/mariadb start
 
+#set up database
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@"localhost" IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_USER}'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
+
 tail -f /dev/null
