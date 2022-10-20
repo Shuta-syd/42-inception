eb6804d01ec3c7a1399c23ba7c6c9a22aafbb930
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-20 15:44:38 +0900

    [mod] /wordpress/conf/wp-config.php

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
+#install wordpress
 wget https://wordpress.org/latest.tar.gz .
 tar -xzvf ./latest.tar.gz
 rm -rf ./latest.tar.gz
 
-
+#setting wp-config.php
+rm -rf /wordpress/wp-config-sample.php
+mv  /var/www/html/wp-config.php /wordpress/
 
 tail -f /dev/null
