239913efce326fab52d139e8e5fa8734a9208061
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-26 23:18:18 +0900

    [fix] wordpress mount error fixed, but temporary

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,9 +6,13 @@ mv latest.tar.gz /var/www
 tar -xzvf /var/www/latest.tar.gz
 rm -rf /var/www/latest.tar.gz
 
+#mv conf files
+rm -rf /etc/php8/php-fpm.d/www.conf
+mv ./www.conf /etc/php8/php-fpm.d/
+
 #setting wp-config.php
 rm -rf /var/www/wordpress/wp-config-sample.php
-mv  /var/www/html/wp-config.php /var/www/wordpress/wp-config.php
+mv  ./wp-config.php /var/www/wordpress/wp-config.php
 
 #start php8-fpm
 /usr/sbin/php-fpm8
