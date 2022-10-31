301e89be58b019b437979d1d3dc4e026705df7f1
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-31 18:20:45 +0900

    [fix] establish database connnect

---
+++ srcs/requirements/mariadb/conf/env.sh
@@ -0,0 +1,1 @@
+sed -i 's/MYSQL_DATABASE/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_USER/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_PASSWORD/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_ROOT_PASSWORD/root_pw/g' create_db.sql
