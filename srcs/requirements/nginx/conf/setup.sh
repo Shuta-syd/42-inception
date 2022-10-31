44f9df2431a479f9d0191204a7d5f551b536a4aa
Shuta-syd <shuta.ogura0921@gmail.com>
2022-10-31 22:50:51 +0900

    [fix] nginx.conf

--- srcs/requirements/nginx/conf/setup.sh
+++ srcs/requirements/nginx/conf/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-cat /etc/hosts | sed "s/127.0.0.1\tlocalhost/127.0.0.1\tshogura.42.fr localhost/g" > /etc/hosts2 
-cat /etc/hosts2 > /etc/hosts
-echo "add shogura.42.fr > /etc/hosts"
+echo "127.0.0.1	shogura.42.fr" >> /etc/hosts
 
-#start nginx
 nginx -g "daemon off;"
